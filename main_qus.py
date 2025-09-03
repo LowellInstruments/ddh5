33833de0b2be420171b081ff5f4cafb119ca86dd
kaz <kaz>
2025-09-03 12:52:32 -0400

    more

--- main_qus.py
+++ main_qus.py
@@ -4,12 +4,15 @@ from gps.gps_quectel import gps_hat_detect_list_of_usb_ports
 
 def main_qus():
     ls = gps_hat_detect_list_of_usb_ports()
+    port_ctrl = ""
     if not ls:
         print(f'\nQUS -> port_GPS = "", port_CTL = ""')
-        return
-    port_nmea = ls[1]
-    port_ctrl = ls[-2]
-    print(f'\nQUS -> port_GPS {port_nmea}, port_CTL: {port_ctrl}')
+    else:
+        port_nmea = ls[1]
+        port_ctrl = ls[-2]
+        print(f'\nQUS -> port_GPS {port_nmea}, port_CTL: {port_ctrl}')
+    with open("/tmp/usb_quectel_ctl", 'w') as f:
+        f.write(port_ctrl)
 
 
 
