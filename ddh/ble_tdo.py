d14f25259af4e09ec77ff426ccde83a6e6bbe02e
agregg <andrew@sednaaero.com>
2026-03-05 18:19:23 -0500

    Fix BLE connection cleanup: ensure ble_disconnect always called
    - Wrap ble_download_tdo in try/finally block
    - Always call ble_disconnect() even on connection/download errors
    - Prevents stale connection state in global LoggerBle instance
    - Fixes issue where failed connection attempts prevent subsequent retries

--- ddh/ble_tdo.py
+++ ddh/ble_tdo.py
@@ -137,213 +137,218 @@ async def ble_download_tdo(d):
     sn = ddh_config_get_logger_sn_from_mac(mac)
     create_path_to_folder_dl_files_from_mac(mac)
 
-
-    rv = await lc.ble_connect_by_dev(dev)
-    _une(not rv, d, "comm.")
-    _rae(not rv, "connecting")
-    lg.a(f"connected to {mac}")
-
-
-    if ddh_ble_logger_needs_a_reset(mac):
-        lg.a(f"warning, logger reset file {mac} found, deleting it")
-        await lc.cmd_rst()
-        # out of here for sure
-        raise BLEAppException("TDO interact logger reset file")
+    try:
+        rv = await lc.ble_connect_by_dev(dev)
+        _une(not rv, d, "comm.")
+        _rae(not rv, "connecting")
+        lg.a(f"connected to {mac}")
 
 
-    rv, v = await lc.cmd_gfv()
-    _rae(rv, "gfv")
-    lg.a(f"GFV | {v}")
-    d['gfv'] = v
+        if ddh_ble_logger_needs_a_reset(mac):
+            lg.a(f"warning, logger reset file {mac} found, deleting it")
+            await lc.cmd_rst()
+            # out of here for sure
+            raise BLEAppException("TDO interact logger reset file")
+
+
+        rv, v = await lc.cmd_gfv()
+        _rae(rv, "gfv")
+        lg.a(f"GFV | {v}")
+        d['gfv'] = v
     
 
-    rv, state = await lc.cmd_sts()
-    _rae(rv, "sts")
-    lg.a(f"STS | logger was {state}")
+        rv, state = await lc.cmd_sts()
+        _rae(rv, "sts")
+        lg.a(f"STS | logger was {state}")
 
 
-    # feature has-logger-been-in-water
-    flag_ignore_hbw = ddh_get_template_of_path_of_hbw_flag_file().format(mac)
-    if exp_debug_skip_hbw() != 1:
-        if state == 'running':
-            if v >= MIN_VERSION_HBW_CMD:
-                if os.path.exists(flag_ignore_hbw):
-                    os.unlink(flag_ignore_hbw)
-                    lg.a('file flag to ignore HBW exists, FORCE download it')
-                else:
-                    # normal HBW command
-                    lg.a('sending command Has-Been-in-Water')
-                    rv, v = await lc.cmd_hbw()
-                    _rae(rv, "hbw")
-                    lg.a(f"HBW | {v}")
-                    if v == 0:
-                        lg.a('logger has NOT been in water, no need to download it')
-                        await lc.ble_disconnect()
-                        return 2
-                    lg.a("logger has been in water, we download it")
+        # feature has-logger-been-in-water
+        flag_ignore_hbw = ddh_get_template_of_path_of_hbw_flag_file().format(mac)
+        if exp_debug_skip_hbw() != 1:
+            if state == 'running':
+                if v >= MIN_VERSION_HBW_CMD:
+                    if os.path.exists(flag_ignore_hbw):
+                        os.unlink(flag_ignore_hbw)
+                        lg.a('file flag to ignore HBW exists, FORCE download it')
+                    else:
+                        # normal HBW command
+                        lg.a('sending command Has-Been-in-Water')
+                        rv, v = await lc.cmd_hbw()
+                        _rae(rv, "hbw")
+                        lg.a(f"HBW | {v}")
+                        if v == 0:
+                            lg.a('logger has NOT been in water, no need to download it')
+                            await lc.ble_disconnect()
+                            return 2
+                        lg.a("logger has been in water, we download it")
+            else:
+                lg.a('logger NOT running, not sending HBW command')
         else:
-            lg.a('logger NOT running, not sending HBW command')
-    else:
-        lg.a("warning: not sending command Has-Been-in-Water, it's disabled in configuration file")
+            lg.a("warning: not sending command Has-Been-in-Water, it's disabled in configuration file")
 
-    rv = await lc.cmd_sws(g)
-    _rae(rv, "sws")
-    lg.a("SWS | OK")
+        rv = await lc.cmd_sws(g)
+        _rae(rv, "sws")
+        lg.a("SWS | OK")
 
-    rv, t = await lc.cmd_utm()
-    _rae(rv, "utm")
-    lg.a(f"UTM | {t}")
+        rv, t = await lc.cmd_utm()
+        _rae(rv, "utm")
+        lg.a(f"UTM | {t}")
 
-    rv, b = await lc.cmd_bat()
-    _rae(rv, "bat")
-    adc_b = b
-    b /= BAT_FACTOR_TDO
-    lg.a(f"BAT | ADC {adc_b} mV -> battery {int(b)} mV")
-    d["battery_level"] = b
-    if adc_b < 982:
-        ln = LoggerNotification(mac, sn, 'TDO', adc_b)
-        notify_logger_error_low_battery(g, ln)
-        app_state_set(EV_BLE_LOW_BATTERY, t_str(STR_EV_BLE_LOW_BATTERY))
-        d['error'] = 'low battery'
-        rv_bad_bat = 1
-        _une(rv_bad_bat, d, "BAT_low_error", ce=1)
-        lg.a(f'BAT | error {rv_bad_bat}')
-        _rae(rv_bad_bat, "bat")
+        rv, b = await lc.cmd_bat()
+        _rae(rv, "bat")
+        adc_b = b
+        b /= BAT_FACTOR_TDO
+        lg.a(f"BAT | ADC {adc_b} mV -> battery {int(b)} mV")
+        d["battery_level"] = b
+        if adc_b < 982:
+            ln = LoggerNotification(mac, sn, 'TDO', adc_b)
+            notify_logger_error_low_battery(g, ln)
+            app_state_set(EV_BLE_LOW_BATTERY, t_str(STR_EV_BLE_LOW_BATTERY))
+            d['error'] = 'low battery'
+            rv_bad_bat = 1
+            _une(rv_bad_bat, d, "BAT_low_error", ce=1)
+            lg.a(f'BAT | error {rv_bad_bat}')
+            _rae(rv_bad_bat, "bat")
 
 
-    rv, v = await lc.cmd_gtm()
-    _rae(rv, "gtm")
-    lg.a(f"GTM | {v}")
+        rv, v = await lc.cmd_gtm()
+        _rae(rv, "gtm")
+        lg.a(f"GTM | {v}")
 
 
-    rv = await lc.cmd_stm()
-    _rae(rv, "stm")
-    lg.a("STM | OK")
+        rv = await lc.cmd_stm()
+        _rae(rv, "stm")
+        lg.a("STM | OK")
 
 
-    # disable log for lower power consumption
-    rv, v = await lc.cmd_log()
-    _rae(rv, "log")
-    if linux_is_rpi():
-        if v != 0:
-            rv, v = await lc.cmd_log()
-            _rae(rv, "log")
-    else:
-        # we want logs while developing
-        if v != 1:
-            rv, v = await lc.cmd_log()
-            _rae(rv, "log")
+        # disable log for lower power consumption
+        rv, v = await lc.cmd_log()
+        _rae(rv, "log")
+        if linux_is_rpi():
+            if v != 0:
+                rv, v = await lc.cmd_log()
+                _rae(rv, "log")
+        else:
+            # we want logs while developing
+            if v != 1:
+                rv, v = await lc.cmd_log()
+                _rae(rv, "log")
 
 
 
-    rv, ls = await lc.cmd_dir()
-    _rae(rv, "dir error " + str(rv))
-    lg.a(f"DIR | {ls}")
+        rv, ls = await lc.cmd_dir()
+        _rae(rv, "dir error " + str(rv))
+        lg.a(f"DIR | {ls}")
 
 
 
-    # iterate files present in logger
-    for name, size in ls.items():
+        # iterate files present in logger
+        for name, size in ls.items():
 
-        # delete zero-bytes files
-        if size == 0:
-            rv = await lc.cmd_del(name)
+            # delete zero-bytes files
+            if size == 0:
+                rv = await lc.cmd_del(name)
+                _rae(rv, "del")
+                continue
+
+
+            # target file to download
+            lg.a(f"downloading file {name}")
+            rv = await lc.cmd_dwg(name)
+            _rae(rv, "dwg")
+
+
+            # download file
+            rv, file_data = await lc.cmd_dwl(int(size))
+            _rae(rv, "dwl")
+            # todo: DWL or DWF
+            lg.a(f"OK downloaded file {name}")
+
+
+            # save file in our local disk
+            del_name = name
+            if ddh_config_does_flag_file_download_test_mode_exist():
+                name = TESTMODE_FILENAME_PREFIX + name
+            path = str(calculate_path_to_folder_within_dl_files_from_mac_address(mac) / name)
+            with open(path, "wb") as f:
+                f.write(file_data)
+
+
+            # add to the output list
+            d['dl_files'].append(path)
+
+            # delete file in logger
+            rv = await lc.cmd_del(del_name)
             _rae(rv, "del")
-            continue
+            lg.a(f"deleted file {del_name}")
 
 
-        # target file to download
-        lg.a(f"downloading file {name}")
-        rv = await lc.cmd_dwg(name)
-        _rae(rv, "dwg")
-
-
-        # download file
-        rv, file_data = await lc.cmd_dwl(int(size))
-        _rae(rv, "dwl")
-        # todo: DWL or DWF
-        lg.a(f"OK downloaded file {name}")
-
-
-        # save file in our local disk
-        del_name = name
-        if ddh_config_does_flag_file_download_test_mode_exist():
-            name = TESTMODE_FILENAME_PREFIX + name
-        path = str(calculate_path_to_folder_within_dl_files_from_mac_address(mac) / name)
-        with open(path, "wb") as f:
-            f.write(file_data)
-
-
-        # add to the output list
-        d['dl_files'].append(path)
-
-        # delete file in logger
-        rv = await lc.cmd_del(del_name)
-        _rae(rv, "del")
-        lg.a(f"deleted file {del_name}")
-
-
-        # create LEF file with download info
-        lg.a(f"creating file LEF for {name}")
-        lef_create_file(g, name)
+            # create LEF file with download info
+            lg.a(f"creating file LEF for {name}")
+            lef_create_file(g, name)
 
 
 
-    # format file-system
-    await asyncio.sleep(.5)
-    rv = await lc.cmd_frm()
-    _rae(rv, "frm")
-    lg.a("FRM | OK")
+        # format file-system
+        await asyncio.sleep(.5)
+        rv = await lc.cmd_frm()
+        _rae(rv, "frm")
+        lg.a("FRM | OK")
 
 
 
-    # check sensor Temperature
-    rv = await lc.cmd_gst()
-    # rv: (0, 46741)
-    bad_rv = not rv or rv[0] == 1 or rv[1] == 0xFFFF or rv[1] == 0
-    if bad_rv:
-        _une(bad_rv, d, "T_sensor_error", ce=1)
-        lg.a(f'GST | error {rv}')
-        d['error'] = 'sensor T'
-    _rae(bad_rv, "gst")
+        # check sensor Temperature
+        rv = await lc.cmd_gst()
+        # rv: (0, 46741)
+        bad_rv = not rv or rv[0] == 1 or rv[1] == 0xFFFF or rv[1] == 0
+        if bad_rv:
+            _une(bad_rv, d, "T_sensor_error", ce=1)
+            lg.a(f'GST | error {rv}')
+            d['error'] = 'sensor T'
+        _rae(bad_rv, "gst")
 
 
 
-    # check sensor Pressure
-    rv = await lc.cmd_gsp()
-    # rv: (0, 1241)
-    bad_rv = not rv or rv[0] == 1 or rv[1] == 0xFFFF or rv[1] == 0
-    if bad_rv:
-        _une(bad_rv, d, "P_sensor_error", ce=1)
-        lg.a(f'GSP | error {rv}')
-        ln = LoggerNotification(mac, sn, 'TDO', b)
-        notify_logger_error_sensor_pressure(g, ln)
-        d['error'] = 'sensor P'
-    _rae(bad_rv, "gsp")
+        # check sensor Pressure
+        rv = await lc.cmd_gsp()
+        # rv: (0, 1241)
+        bad_rv = not rv or rv[0] == 1 or rv[1] == 0xFFFF or rv[1] == 0
+        if bad_rv:
+            _une(bad_rv, d, "P_sensor_error", ce=1)
+            lg.a(f'GSP | error {rv}')
+            ln = LoggerNotification(mac, sn, 'TDO', b)
+            notify_logger_error_sensor_pressure(g, ln)
+            d['error'] = 'sensor P'
+        _rae(bad_rv, "gsp")
 
 
 
-    # reconfigure the logger settings
-    await _tdo_reconfigure_profiling(d['gfv'])
+        # reconfigure the logger settings
+        await _tdo_reconfigure_profiling(d['gfv'])
 
 
-    # wake mode
-    rerun_flag = not ddh_does_do_not_rerun_file_flag_exist()
-    w = "on" if rerun_flag else "off"
-    rv = await lc.cmd_wak(w)
-    _rae(rv, "wak")
-    lg.a(f"WAK | {w} OK")
+        # wake mode
+        rerun_flag = not ddh_does_do_not_rerun_file_flag_exist()
+        w = "on" if rerun_flag else "off"
+        rv = await lc.cmd_wak(w)
+        _rae(rv, "wak")
+        lg.a(f"WAK | {w} OK")
 
 
-    # re-run the logger or not
-    d['rerun'] = rerun_flag
-    if rerun_flag:
-        rv = await lc.cmd_rws(g)
-        if rv:
-            d['error'] = 'running'
-        _rae(rv, "rws")
-        lg.a("RWS | OK")
+        # re-run the logger or not
+        d['rerun'] = rerun_flag
+        if rerun_flag:
+            rv = await lc.cmd_rws(g)
+            if rv:
+                d['error'] = 'running'
+            _rae(rv, "rws")
+            lg.a("RWS | OK")
 
-
-    await lc.ble_disconnect()
-    return 0
+        return 0
+    
+    finally:
+        # Always disconnect, even on error, to prevent stale connection state
+        try:
+            await lc.ble_disconnect()
+        except Exception as e:
+            lg.a(f"warning: error during disconnect: {e}")
